c2dc701cd8c8a2d677b273f3b7361d9d6c9792d3
polinakryukova <polinakpa@gmail.com>
2024-11-07 00:29:06 +0300

    TASK2

---
+++ UrbanDjango/task2/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+# Create your views here.
+def index(request):
+    return render(request, 'second_task/func_template.html')
+
+
+# class index2(TemplateView):
+#     template_name = 'second_task/class_template.html'
